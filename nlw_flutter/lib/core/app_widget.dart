1c8cca40c947cc74ae2c25d532878d4826282551
gustavoaesteves <gustavoalves19992@gmail.com>
2026-07-10 10:46:51 -0300

    nlw: appbar, score and titles

--- nlw_flutter/lib/core/app_widget.dart
+++ nlw_flutter/lib/core/app_widget.dart
@@ -1,11 +1,9 @@
+import 'package:DevQuiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "DevQuiz",
-      home: Scaffold(),
-    );
+    return MaterialApp(title: "DevQuiz", home: HomePage());
   }
 }
