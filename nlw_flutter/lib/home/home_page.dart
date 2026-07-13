34d315c1c5516db732758a06d4f6ee0af168fd55
gustavoaesteves <gustavoalves19992@gmail.com>
2026-07-13 17:51:35 -0300

    nlw: home with level buttons and quiz card

--- nlw_flutter/lib/home/home_page.dart
+++ nlw_flutter/lib/home/home_page.dart
@@ -1,4 +1,6 @@
 ﻿import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,7 +14,43 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(),
-    );
+        appBar: AppBarWidget(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    LevelButtonWidget(label: "Fácil"),
+                    LevelButtonWidget(label: "Médio"),
+                    LevelButtonWidget(label: "Difícil"),
+                    LevelButtonWidget(label: "Perito"),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  children: [
+                    QuizCardWidget(),
+                    QuizCardWidget(),
+                    QuizCardWidget(),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
