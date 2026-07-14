94ac6941a14db714afef76dd108f99a5a4eb1922
gustavoaesteves <gustavoalves19992@gmail.com>
2026-07-14 10:28:01 -0300

    nlw: page question, listView, widget progress

---
+++ nlw_flutter/lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,37 @@
+﻿import 'package:DevQuiz/challenge/widgets/awnser/awnser_widget.dart';
+import 'package:DevQuiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+  const QuizWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.heading,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          AwnserWidget(
+              title:
+                  "Teste de quesaaaaaaaaaaaaaaaaaaaad asdasdasd asda dsd asdasdasddsgf asdf sdfgão"),
+          AwnserWidget(
+              title:
+                  "Teste de quesaaaaaaaaaaaaaaaaaaaad asdasdasd asda dsd asdasdasddsgf asdf sdfgão"),
+          AwnserWidget(
+              title:
+                  "Teste de quesaaaaaaaaaaaaaaaaaaaad asdasdasd asda dsd asdasdasddsgf asdf sdfgão"),
+          AwnserWidget(
+              title:
+                  "Teste de quesaaaaaaaaaaaaaaaaaaaad asdasdasd asda dsd asdasdasddsgf asdf sdfgão"),
+        ],
+      ),
+    );
+  }
+}
--- nlw_flutter/lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ nlw_flutter/lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,4 +1,5 @@
 ﻿import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -34,15 +35,7 @@ class QuizCardWidget extends StatelessWidget {
             children: [
               Expanded(
                   flex: 1, child: Text("3 de 10", style: AppTextStyles.body11)),
-              Expanded(
-                flex: 2,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor:
-                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-                ),
-              )
+              Expanded(flex: 2, child: ProgressIndicatorWidget(value: 0.3))
             ],
           )
         ],
